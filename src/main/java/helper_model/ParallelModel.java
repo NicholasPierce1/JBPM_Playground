005b81e894a5043e2b06ff109f7c978b4b880604
Nicholas Pierce <nickpierce7898@gmail.com>
2021-11-04 12:01:49 -0500

    rectified concerrency controls to bar race conditions that occur with individual lock acquiring on threads

--- src/main/java/helper_model/ParallelModel.java
+++ src/main/java/helper_model/ParallelModel.java
@@ -2,12 +2,19 @@ package helper_model;
 
 import java.io.Serializable;
 import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public final class ParallelModel implements Serializable {
 	
 	private static transient final ParallelModel SHARED = new ParallelModel();
 	
 	private transient final List<? extends Thread> THREADS = new ArrayList<MyThread>();
+	
+	private transient final ExecutorService EXECUTOR_SERVICE = Executors.newFixedThreadPool(2);
+	
+	private transient final List<Future<?>> FUTURES = new ArrayList<Future<?>>();
 	
 	private ParallelModel() {}
 	
@@ -16,7 +23,7 @@ public final class ParallelModel implements Serializable {
 	}
 	
 	@SuppressWarnings("unchecked")
-	public void await(final String componentName) {
+	public void awaitBad(final String componentName) {
 		
 		System.out.println(componentName + " is starting\t" + Thread.currentThread().getName());
 		
@@ -31,17 +38,61 @@ public final class ParallelModel implements Serializable {
 //		}
 	}
 	
-	public void awaitThreads() {
+	public void awaitThreadsBad() {
 		this.THREADS.stream().parallel().forEach((thread)-> {
 			try {
 				synchronized(thread) {
 					thread.wait();
+					thread.notifyAll();
 				}
 			}
 			catch(InterruptedException e) {
 				System.out.println("error uncovered: " + e.getLocalizedMessage());
 			}
 		});
+		System.out.println("exiting");
+	}
+	
+	public void await(final String componentName) {
+		
+		System.out.println(componentName + " is starting\t" + Thread.currentThread().getName());
+		
+		try {
+			this.FUTURES.add( 
+					this.EXECUTOR_SERVICE.submit(
+						() -> {
+							int secondsToWait = new Random().nextInt(5);
+							
+							try {
+								Thread.sleep(secondsToWait * 1000);
+							}
+							catch(InterruptedException e) {
+								System.out.println("error uncovered: " + e.getLocalizedMessage());
+							}
+							finally {
+								final StringBuilder outgoingText = new StringBuilder(componentName);
+								System.out.println(outgoingText.append(" is finished"));
+							}
+						}
+				)
+			);
+		}
+		catch(Exception ex) {
+			System.out.println("error uncovered: " + ex.getLocalizedMessage());
+		}
+	}
+	
+	public void awaitThreads() {
+		this.FUTURES.parallelStream().forEach(
+				(future) -> {
+					try {
+						future.get();
+					}
+					catch(Exception ex) {
+						System.out.println(ex.getLocalizedMessage());
+					}
+				}
+			);
 	}
 	
 	static final class MyThread extends Thread{
